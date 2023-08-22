6cfc4c3066304a7efb926c47f259c26b2fb23c04
JChavannes01 <janko.chavannes@gmail.com>
2023-08-22 21:44:40 +0200

    exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 
 const StatisticLine = ({ text, value }) => (
-  <p>
-    {text} {value}
-  </p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 );
 
 const Statistics = ({ good, neutral, bad }) => {
@@ -12,7 +13,7 @@ const Statistics = ({ good, neutral, bad }) => {
   }
 
   return (
-    <>
+    <table>
       <StatisticLine text="good" value={good} />
       <StatisticLine text="neutral" value={neutral} />
       <StatisticLine text="bad" value={bad} />
@@ -25,7 +26,7 @@ const Statistics = ({ good, neutral, bad }) => {
         text="positive"
         value={`${(100 * good) / (good + neutral + bad)} %`}
       />
-    </>
+    </table>
   );
 };
 
